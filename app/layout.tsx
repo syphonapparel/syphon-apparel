c72670fcd87c220264af00f1d2d437c942a12d4d
Ahmad Tallal <suppor@syphonapparel.com>
2026-07-17 01:12:23 +0500

    Add GA4 integration and improve SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -176,6 +177,8 @@ export default function RootLayout({
 
         {children}
       </body>
+
+      <GoogleAnalytics gaId="G-85CY1ED1CS" />
     </html>
   );
 }
